648127c7a83cad50436e5bc3031fd4df4d98c212
Ariel Meshorer <ariel.meshorer@nbcuni.com>
2024-10-17 16:07:42 -0400

    completed first standofrd lecture

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -9,13 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack{
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
         }
+        .foregroundColor(.orange)
+        .imageScale(.small)
         .padding()
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false // defualt Vlaue
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                // Z vs H vs V
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+                // something that behaves like a shape
+                Text("👻").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+                    // styles perfolate all the way down, orange is the default from hstack and it goes all the way down to cardview. because the background isn't specified, it gets the background color from above.
+               
+            }
+        }
     }
 }
 
