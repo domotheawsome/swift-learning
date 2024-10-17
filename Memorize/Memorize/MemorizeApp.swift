0b1c6e0709fb6c9bc91bc62fa0a3a4d0a41b21d6
Ariel Meshorer <ariel.meshorer@nbcuni.com>
2024-10-17 16:06:26 -0400

    completed first stanford lecture

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Meshorer, Ariel (604555397) on 10/17/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
