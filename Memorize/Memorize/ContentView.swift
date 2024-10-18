edb79483706cd656d638e0b8257fe878d85470fd
Ariel Meshorer <ariel.meshorer@nbcuni.com>
2024-10-18 15:23:09 -0400

    commiting lecture 2 part 1

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -22,23 +22,42 @@ struct ContentView: View {
 }
 
 struct CardView: View {
-    var isFaceUp: Bool = false // defualt Vlaue
-    
+    @State var isFaceUp = false // defualt Vlaue
+    // way to declare state in swift
     var body: some View {
         ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            
+            // let means its a constant
+            // var means its a variable/it varies
+            // inside a viewbuilder (zstack) you always use let because it never changes
+            // type inference = becuase you assigned the let to a rounded rectangle, it will infer that you want it to be a rounded rectangle
+            // we always let swift infer rather than explicity typing it ourselves
+            // swift is a strongly typed language
+                // it knows all the types at compile time
+                // very strongly typed language are annoying if you have to be constantly be expressing types
+                // the tradeoff is that it does enormous type inference for being a strongly typed language
             if isFaceUp {
-                // Z vs H vs V
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 2)
-                // something that behaves like a shape
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
                 Text("👻").font(.largeTitle)
+            
             } else {
                 RoundedRectangle(cornerRadius: 12)
                     // styles perfolate all the way down, orange is the default from hstack and it goes all the way down to cardview. because the background isn't specified, it gets the background color from above.
                
             }
+               
+        }.onTapGesture {
+            print("tapped")
+            // prints to the preview
+            // very convienent
+            
+            isFaceUp.toggle() // this will flip the card
+            // views are immutable -- self is immutable
+            
+            // backend is what will flip cards over, you cannot do it on the view
+            // toggle is a better way to flip cards
         }
     }
 }
